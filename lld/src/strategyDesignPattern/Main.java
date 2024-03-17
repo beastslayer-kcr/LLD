ecad13a8974751d7d0e2fcd197ca9af62ceaaabd
krishna <gopaljirai1999.com>
2024-03-18 02:29:19 +0530

    calling all ayment strategy from main method

--- lld/src/strategyDesignPattern/Main.java
+++ lld/src/strategyDesignPattern/Main.java
@@ -2,8 +2,19 @@ package strategyDesignPattern;
 
 public class Main {
 	public static void main(String[] args) {
+		
+		//credit card payment 
 		CreditCardPaymentStrategy paymentStrategy = new CreditCardPaymentStrategy();
 		PaymentProcessor paymentProcessor = new PaymentProcessor(paymentStrategy);
 		paymentProcessor.processPayment(10000.00);
+		
+		//Debit card Payment
+		PaymentProcessor DebitpaymentProcessor = new PaymentProcessor( new DebitCardPaymentStartegy());;
+		DebitpaymentProcessor.processPayment(9999.00);
+		
+		//paypal Payment
+		PaymentProcessor paypalDesignPattern = new PaymentProcessor( new PaypalDesignPattern());;
+		paypalDesignPattern.processPayment(8888.00);
+		
 	}
 }
